f037b48877fd07efe9b93f06ee353e1ccbcc7d1b
was <zxt_zel_npl@163.com>
2018-01-18 17:00:08 +0800

    snow

--- jQuery/Rolling.js
+++ jQuery/Rolling.js
@@ -1,6 +1,5 @@
-
 /**
- * 计算每天增加人数
+ * 名单垂直滚动
  * @param rollData {object}
  * object.showBox
  * object.moveBox
@@ -8,39 +7,47 @@
  * object.speed
  */
 
-function rolling(rollData) {
-    var $showBox = $(rollData.showBox);
-    var $moveBox = $showBox.find(rollData.moveBox);
-    var $box = $moveBox.find(rollData.box);
-    var showHeight=$showBox.height();
-    var boxHeight = $box.height();
-    var boxNumbers = $box.length;
-    var repeatNumber = Math.ceil(showHeight / boxHeight);
-    var clones = $moveBox.find(rollData.box+":lt(" + repeatNumber + ")").clone();
+function rolling (rollData) {
+  var $showBox = $(rollData.showBox); //Box
+  var $moveBox = $showBox.find(rollData.moveBox);  //内部包裹
+  var $box = $moveBox.find(rollData.box);  //每个Item
+  var showHeight = $showBox.height();  // Box高度
+  var boxHeight = $box.height();  //每个Item高度
+  var boxNumbers = $box.length;  // Item的个数
 
-    $moveBox.append(clones);
+  /**
+   * 如果高度不满，则不滚动
+   * */
+  if(boxNumbers*boxHeight<=showHeight){
+    return false
+  }
 
+  var repeatNumber = Math.ceil(showHeight / boxHeight);
+  var clones = $moveBox.find(rollData.box + ":lt(" + repeatNumber + ")").clone();
+
+  $moveBox.append(clones);
+
+  $moveBox.css({
+    '-webkit-transition': 'all ' + rollData.speed + 'ms linear',
+    'transition': 'all ' + rollData.speed + 'ms linear',
+    '-webkit-transform': 'translateY(-' + boxHeight * boxNumbers + 'px)',
+    'transform': 'translateY(-' + boxHeight * boxNumbers + 'px)'
+  });
+
+  $moveBox.on('transitionend webkitTransitionEnd', function () {
     $moveBox.css({
+      '-webkit-transition': '',
+      'transition': '',
+      'transform': '',
+      '-webkit-transform': ''
+    });
+    setTimeout(function () {
+      $moveBox.css({
         '-webkit-transition': 'all ' + rollData.speed + 'ms linear',
         'transition': 'all ' + rollData.speed + 'ms linear',
         '-webkit-transform': 'translateY(-' + boxHeight * boxNumbers + 'px)',
         'transform': 'translateY(-' + boxHeight * boxNumbers + 'px)'
-    });
-
-    $moveBox.on('transitionend webkitTransitionEnd', function () {
-        $moveBox.css({
-            '-webkit-transition': '',
-            'transition': '',
-            'transform': '',
-            '-webkit-transform': ''
-        });
-        setTimeout(function () {
-            $moveBox.css({
-                '-webkit-transition': 'all ' + rollData.speed + 'ms linear',
-                'transition': 'all ' + rollData.speed + 'ms linear',
-                '-webkit-transform': 'translateY(-' + boxHeight * boxNumbers + 'px)',
-                'transform': 'translateY(-' + boxHeight * boxNumbers + 'px)'
-            });
-        })
-    });
+      });
+    })
+  });
 }
