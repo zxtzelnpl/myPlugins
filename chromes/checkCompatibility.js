71b9f03c50a04a50efbc918e24efe0d04a2dd906
was <zxt_zel_npl@163.com>
2018-01-24 14:32:04 +0800

    add compatibility

---
+++ chromes/checkCompatibility.js
@@ -0,0 +1,59 @@
+'use strict'
+
+function compatibility(fun){
+  // var reg={
+  //   test:/texta/gi,
+  //   edge:/Edge\/([0-9]*)\.([0-9]*)/gi,
+  //   firefox:/Firefox\/([0-9]*)\.([0-9]*)/gi,
+  //   safari:/Safari\/([0-9]*)\.([0-9]*)\.([0-9]*)/gi,
+  //   version:/Version\/([0-9]*)\.([0-9]*)\.([0-9]*)/gi,
+  //   chrome:/Chrome\/([0-9]*)/gi
+  // };
+  // var str = navigator.userAgent
+  var str =__str.s360.ie;
+  var reg={
+    edge:/Edge\/([0-9.]*)/gi,
+    firefox:/Firefox\/([0-9.]*)/gi,
+    safari:/Safari\/([0-9.]*)/gi,
+    version:/Version\/([0-9.]*)/gi,
+    chrome:/Chrome\/([0-9.]*)/gi
+  };
+
+  var result={},num;
+  result.edge = reg.edge.exec(str);
+  if(result.edge){
+    console.info('edge is true')
+    return true
+  }
+  result.firefox = reg.firefox.exec(str);
+  if(result.firefox){
+    num = parseInt(result.firefox[1].split('.')[0]);
+    if(num>55){
+      console.log('firefox is true')
+      return true
+    }
+  }
+
+  result.safari = reg.safari.exec(str);
+  result.version = reg.version.exec(str);
+  if(result.safari&&result.version){
+    num = parseInt(result.version[1].split('.')[0]);
+    if(num>5){
+      console.log('safari is true')
+      return true
+    }
+
+  }
+
+  result.chrome = reg.chrome.exec(str);
+  if(result.chrome){
+    num = parseInt(result.chrome[1].split('.')[0]);
+    if(num>=49){
+      console.log('chrome is true')
+      return true
+    }
+  }
+
+  console.warn('不兼容');
+  console.log(result)
+}
---
+++ chromes/compatibility.js
@@ -0,0 +1,46 @@
+'use strict'
+
+function compatibility (fun) {
+  var str = navigator.userAgent
+      , reg = {
+          edge: /Edge\/([0-9.]*)/gi,
+          firefox: /Firefox\/([0-9.]*)/gi,
+          safari: /Safari\/([0-9.]*)/gi,
+          version: /Version\/([0-9.]*)/gi,
+          chrome: /Chrome\/([0-9.]*)/gi
+        }
+      , result = {}
+      , num;
+  result.edge = reg.edge.exec(str);
+  if (result.edge) {
+    return true
+  }
+  result.firefox = reg.firefox.exec(str);
+  if (result.firefox) {
+    num = parseInt(result.firefox[1].split('.')[0]);
+    if (num > 55) {
+      return true
+    }
+  }
+
+  result.safari = reg.safari.exec(str);
+  result.version = reg.version.exec(str);
+  if (result.safari && result.version) {
+    num = parseInt(result.version[1].split('.')[0]);
+    if (num > 5) {
+      return true
+    }
+
+  }
+
+  result.chrome = reg.chrome.exec(str);
+  if (result.chrome) {
+    num = parseInt(result.chrome[1].split('.')[0]);
+    if (num >= 49) {
+      return true
+    }
+  }
+
+  console.warn('不兼容');
+  fun()
+}
