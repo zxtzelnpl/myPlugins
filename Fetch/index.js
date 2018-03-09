0a3a2d024897a4c6ff1c890aa2930e0d57db29ff
was <zxt_zel_npl@163.com>
2018-03-09 10:37:58 +0800

    add something about fetch

--- Fetch/index.js
+++ Fetch/index.js
@@ -1,23 +1,55 @@
 'use strict'
 
-// let myHeaders = new Headers()
-// myHeaders.append('Content-Type', 'application/x-www-form-urlencoded')
-let body = 'name=zxt&age=18'
-let init={
-  method:'GET',
-  mode: 'no-cors', //是否跨域
-  headers:myHeaders,
-  credentials:'include',  //默认是omit，改成include则会带cookie
-  cache: 'default', //缓存
+const url = 'www.zxt.com'
 
-  body:body
+/**第一种写法**/
+{
+  let myHeaders = new Headers()
+  myHeaders.append('Content-Type', 'application/x-www-form-urlencoded')
+  let body = 'name=zxt&age=18'
+  let init={
+    method:'GET',
+    mode: 'no-cors', //是否跨域
+    headers:myHeaders,
+    credentials:'include',  //默认是omit，改成include则会带cookie
+    cache: 'default', //缓存
+    body:body
+  }
+  fetch(url,init).then(res=>res.json).then(json=>{console.log(json)})
 }
-let myHeaders={
-  'Content-Type':'application/x-www-form-urlencoded'
+
+
+
+
+
+
+/**第二种写法**/
+{
+  let myHeaders={
+    'Content-Type':'application/json'
+  }
+  let value={name:'zxt',age:18}
+  let init={
+    method: 'POST',
+    credentials: 'include',
+    body: JSON.stringify(value),
+    headers:myHeaders
+  }
+  fetch(url,init).then(res=>res.json).then(json=>{console.log(json)})
 }
-fetch(url,init)
-    .then(res=>res.json)
-    .then(json=>{
-      console.log(json)
-    })
+
+
+/**第三种写法**/
+{
+  let value={name:'zxt',age:18}
+  let myHeaders = new Headers()
+  myHeaders.append('Content-Type', 'application/json')
+  let request = new Request(url, {
+  method: 'POST',
+  credentials: 'include',
+  body: JSON.stringify(value),
+  headers: myHeaders
+  });
+  fetch(request).then(res=>res.json).then(json=>{console.log(json)})
+}
 
